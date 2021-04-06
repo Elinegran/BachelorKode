e428df02e1b25f1c829124857e6f3ab12b24a5c7
Anne-Ragnhild Einbu <anneragnhild@gmail.com>
2021-04-06 11:47:29 +0200

    gruppemeld

--- Backend/app/controllers/meldinger.js
+++ Backend/app/controllers/meldinger.js
@@ -65,41 +65,39 @@ exports.getMinSamtale = function(req, res)  {
 // Funksjon som sender melding til ALLE brukerne
 exports.MeldingTilAlle = function(req, res) {
 
-  /* const items = [
-    {mottaker: 2, avsender: 13, melding: 'Hei alle sammen fra Berit'},
-    {mottaker: 3, avsender: 13, melding: 'Hei alle sammen fra Berit'},
-    {mottaker: 4, avsender: 13, melding: 'Hei alle sammen fra Berit'}
-  ]; */
-
-  //const mottaker = 4; 
-  //const avsender = req.body.avsender; // Henter avsender (den innloggede) fra frontend. Funker!
-  //const melding = req.body.melding; // Henter medlingsteksen fra Frontend. Funker!
-
   const meldingTilAlle = `INSERT INTO melding(mottaker, avsender, melding) VALUES ?`;
   
-  var values = [
-    [2, 13, 'God påaske, hilsen Berit'],
-    [3, 13, 'God påske, hilsen Berit'],
-    [4, 13, 'God påske, hilsen Berit']
-    
-];
-db.query(meldingTilAlle, [values], function(err) {
-    if (err) throw err;
-    
-});
-  
-  // db.query(meldingTilAlle, [mottaker, avsender, melding], (err,result) => {
-  // db.query(meldingTilAlle, [items.map(item => [item.mottaker, item.avsender, item.melding])], (err,result) => {
-    /* if (err) {
+  var mottaker = [2,3,4];
+  var avsender = req.body.avsender; // Henter avsender (den innloggede) fra frontend. Funker!
+  var melding = req.body.melding; // Henter medlingsteksen fra Frontend. Funker!
+ 
+  // Dette formatet funker!
+  /* var brukere = [
+    [2, avsender, melding],
+    [3, avsender, melding],
+    [4, avsender, melding] 
+  ]; */
+
+  var brukere = mottaker.map(bruker => [bruker.mottaker, avsender, melding]);
+  console.log(brukere); 
+
+  //var liste = brukere.map (mottaker => [mottaker.mottakere, mottaker.avsender, mottaker.melding ] ); 
+
+  /* db.query(meldingTilMedlemmer, [mottaker, avsender, melding], (err,result) => {
+    if (err) {
       console.log(err)
     }
     else{
       res.send(result);
     }
-  });
-}; */
+  }); */
 
-};
+  // Legger alle meldingene inn i DB (Denne funker!)
+  db.query(meldingTilAlle, [brukere], function(err) {
+      if (err) throw err;   
+  });
+  
+}; // Slutt på funksjon meldingTilAlle()
 
 
 // Funksjon som sender melding til ALLE brukerne i en GRUPPE
@@ -107,7 +105,7 @@ exports.Gruppemelding = function(req, res) {
 
   // Liste med alle meldingene
   const medlemmer = [
-    [13, 13, 'Melding til alle fra Berit'],
+    [2, 13, 'Melding til alle fra Berit'],
     [3, 13, 'Melding til alle fra Berit'],
     [4, 13, 'Melding til alle fra Berit']
   ];
