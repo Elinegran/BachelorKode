e691e1313827250017589037b684f434058869de
katrinenl <katrine.nerhus@hotmail.com>
2021-04-14 15:54:22 +0200

    design

--- Frontend/src/components/Meldinger/samtaler.js
+++ Frontend/src/components/Meldinger/samtaler.js
@@ -4,10 +4,7 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Accordion, Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'; 
 import AuthService from '../../services/auth.service'; 
-import Skrivemelding from './skriveMelding';
 import Skrivemeldinger from '../../components/Meldinger/skriveMelding.js';
-import Meldingsliste from './innboks.js'; 
-import SimpleDateTime  from 'react-simple-timestamp-to-date'; // Formatere tid og dato
 import moment from 'moment';
 
 
@@ -40,8 +37,6 @@ export default class Samtaleliste extends React.Component {
   }
 
 render() {
-  // let dagensDato = moment(new Date()).format("YYYY-MM-DDTHH:MM:00.0000Z");
-  let tidLest = moment(new Date()).format("DD-MM-YYYY HH:MM");
 
   return (
     <p>
@@ -52,16 +47,12 @@ render() {
         <Card.Title><h2>{melding.fornavn} {melding.etternavn}</h2></Card.Title>
         <Card.Text>
           <p>{melding.melding}</p>
-          <p><SimpleDateTime dateFormat="DMY" timeFormat="HMA" dateSeparator="." timeSeparator=":" showTime="1" showDate="1" >
-            {melding.tid}</SimpleDateTime></p>
+          <p>{moment(melding.tid).format("DD-MM-YYYY HH:mm")}</p>
           <p> {melding.meldingLest == '0000-00-00 00:00:00' ? null : 
               <span class="badge badge-pill badge-success">
-                Meldingen er lest {moment(melding.meldingLest).format("DD-MM-YYYY HH:MM")}                
+                Meldingen er lest {moment(melding.meldingLest).format("DD-MM-YYYY HH:mm")}                
               </span>}
-          </p>
-    //              {/* {<SimpleDateTime dateFormat="DMY" timeFormat="HMA" dateSeparator="." timeSeparator=":" showTime="1" showDate="1" >
-    // {melding.meldingLest}</SimpleDateTime>}</span>} */}
-              
+          </p> 
           
         </Card.Text>
       </Card.Body>
