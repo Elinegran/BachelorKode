2ba2d2543bc5cc108fd10d57e6a266a7c0866837
Anne-Ragnhild Einbu <anneragnhild@gmail.com>
2021-04-28 09:11:28 +0200

    db connection

--- Backend/app/config/config.js
+++ Backend/app/config/config.js
@@ -1,5 +1,12 @@
 const mysql = require("mysql");
 
+// const db = mysql.createPool({
+//   host: "mbdigitalno01.mysql.domeneshop.no",
+//   user: "mbdigitalno01",
+//   password: "9-Stank-tulla-Hogge-hang",
+//   database: "mbdigitalno01",
+// });
+
 const db = mysql.createPool({
   host: "mbdigitalno01.mysql.domeneshop.no",
   user: "mbdigitalno01",
